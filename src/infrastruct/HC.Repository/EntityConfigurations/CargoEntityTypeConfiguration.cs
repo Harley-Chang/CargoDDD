4f99208ee5cdf300af62ad771acaf758cd094933
Harley-Chang <blzhangvip@outlook.com>
2023-03-30 07:59:07 +0800

    docs:Add note for EntityTypeConfiguration.

--- src/infrastruct/HC.Repository/EntityConfigurations/CargoEntityTypeConfiguration.cs
+++ src/infrastruct/HC.Repository/EntityConfigurations/CargoEntityTypeConfiguration.cs
@@ -14,12 +14,14 @@ public class CargoEntityTypeConfiguration : IEntityTypeConfiguration<Cargo>
             .IsRequired();
 
         builder.Property(s => s.Name).HasMaxLength(200);
+        //从属实体类型属性
         builder.OwnsOne(s => s.RouteSpecification, a =>
         {
             a.Property(b => b.Origin).HasMaxLength(500);
             a.Property(b => b.Destination).HasMaxLength(500);
         });
 
+        //将枚举转为字符串，增加可视化、可维护性
         builder.Property(s => s.HazMatCode).HasMaxLength(50)
             .IsUnicode(false).HasConversion<string>();
     }
